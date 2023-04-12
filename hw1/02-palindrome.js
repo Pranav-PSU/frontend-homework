2b6beff3a4082af42865d54cd801f5f57ed924b6
Pranav-PSU <pran4@pdx.edu>
2023-04-12 14:49:09 -0700

    Homework 1 completed

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,49 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
+const resultPalindrome = document.querySelector(".resultPalindrome");
 
-elem.addEventListener('input', handleInput);
+function handleInput(event) {
+  event.preventDefault();
+  resultPalindrome.innerHTML = "";
+
+  const userInputValue = parseInt(elem.value);
+
+  if (Number.isNaN(userInputValue)) {
+    resultPalindrome.innerHTML = "";
+    return;
+  }
+
+  if (userInputValue < 0) {
+    resultPalindrome.innerHTML =
+      '<p class="text-danger">Please enter a positive number.</p>';
+    return;
+  }
+
+  const reversedValue = checkPalindrome(userInputValue);
+
+  if (reversedValue) {
+    resultPalindrome.innerHTML =
+      '<p class="text-success">Yes. This is a palindrome!</p>';
+  } else {
+    resultPalindrome.innerHTML = '<p class="text-danger">No. Try again.</p>';
+  }
+}
+
+let checkPalindrome = (inputValue) => {
+  let reversedNumber = 0;
+  let tempNumber = inputValue;
+  let isPalindrome = false;
+
+  while (tempNumber > 0) {
+    const remainder = tempNumber % 10;
+    reversedNumber = reversedNumber * 10 + remainder;
+    tempNumber = Math.floor(tempNumber / 10);
+  }
+
+  if (inputValue === reversedNumber) {
+    isPalindrome = true;
+  }
+
+  return isPalindrome;
+};
+
+elem.addEventListener("input", handleInput);
