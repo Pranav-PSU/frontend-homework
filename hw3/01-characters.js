b5dbe2880f9723bcb560dc27a8cf78aac5257384
Pranav-PSU <pran4@pdx.edu>
2023-06-05 22:00:36 -0700

    Made changes in the code as per new suggestions  and pushed

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,49 +1,47 @@
 async function addCharacters(charactersInformation) {
-  const dataContainerElement = document.querySelector('.dataContainer');
-  const row = document.createElement('div');
-  row.className = 'row';
+  const dataContainerElement = document.querySelector(".dataContainer");
+  const row = document.createElement("div");
+  row.className = "row";
 
   charactersInformation.forEach((character) => {
     // Creating the main div with Bootstrap column classes
-    const parentDivElement = document.createElement('div');
-    parentDivElement.className = 'col-6 col-md-4 col-lg-3 mt-2 mx-0';
+    const parentDivElement = document.createElement("div");
+    parentDivElement.className = "col-6 col-md-4 col-lg-3 mt-2 mx-0";
 
     // div element for an image
-    const imageDivEement = document.createElement('div');
-    imageDivEement.className = 'pt-2 justify-content-center mx-auto imageDivEementClass';
-    imageDivEement.style = 'text-align: center; width: 190px; height: 340px; margin-top: 20px;  padding: 0px;';
+    const imageDivEement = document.createElement("div");
+    imageDivEement.className =
+      "pt-2 justify-content-center mx-auto imageDivEementClass";
 
     // Add hover background color change
-    imageDivEement.addEventListener('mouseover', () => {
-      imageDivEement.style.backgroundColor = 'var(--blue)';
-      imageDivEement.style.color = 'var(--white)';
+    imageDivEement.addEventListener("mouseover", () => {
+      imageDivEement.style.backgroundColor = "var(--blue)";
+      imageDivEement.style.color = "var(--white)";
     });
 
-    imageDivEement.addEventListener('mouseout', () => {
-      imageDivEement.style.backgroundColor = '';
-      imageDivEement.style.color = 'var(--black)';
+    imageDivEement.addEventListener("mouseout", () => {
+      imageDivEement.style.backgroundColor = "";
+      imageDivEement.style.color = "var(--black)";
     });
 
     // Creating the image
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     img.src = character.imageUrl;
-    img.style = 'width: 170px; height: 200px; object-fit: cover;';
+    img.className = "imageElement";
     img.alt = `Portrait of ${character.fullName}`;
 
     // Creating h2 element for name of the character
-    const characterName = document.createElement('h2');
-    characterName.className = 'mt-3 fw-bold text-center mx-auto';
+    const characterName = document.createElement("h2");
+    characterName.className = "mt-3 fw-bold text-center mx-auto";
     characterName.textContent = character.fullName;
 
     // Creating h6 element for title of the character
-    const characterTitle = document.createElement('h6');
-    characterTitle.className = 'mt-1 fw-bold text-center mx-auto';
+    const characterTitle = document.createElement("h6");
+    characterTitle.className = "mt-1 fw-bold text-center mx-auto";
     characterTitle.textContent = character.title;
 
     // Appending all the custom elements in their respective parent div
-    imageDivEement.appendChild(img);
-    imageDivEement.appendChild(characterName);
-    imageDivEement.appendChild(characterTitle);
+    imageDivEement.append(img, characterName, characterTitle);
     parentDivElement.appendChild(imageDivEement);
 
     // Appending parent div element in the row
@@ -61,9 +59,10 @@ function fetchData(apiUrl, callback) {
       callback(data);
     })
     .catch((error) => {
-      console.error('Fetch Failed', error);
+      console.log("Fetch Failed:", error);
+      alert("Fetch Failed: Something went wrong", error);
     });
 }
 
-const apiUrl = 'https://thronesapi.com/api/v2/Characters';
+const apiUrl = "https://thronesapi.com/api/v2/Characters";
 fetchData(apiUrl, addCharacters);
